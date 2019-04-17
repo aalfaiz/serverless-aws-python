7481222c54f6727e50a138e5221ef6817162cb24
al faiz <aalfaiz555@gmail.com>
2019-04-17 18:12:53 +0700

    init project

---
+++ dynamo_table_creation.py
@@ -0,0 +1,33 @@
+import boto3
+
+def create_dynamo_table(table_name_value, enable_streams=False,
+                        read_capacity=1,
+                        write_capacity=1,
+                        region='ap-southeast-1'):
+    table_name = table_name_value
+    print('creating table: '+table_name)
+    try:
+        client = boto3.client(service_name='dynamodb', region_name=region)
+        print(client.create_table(TableName=table_name,
+                                    AttributeDefinitions=[{'AttributeName':'EventId',
+                                                         'AttributeType' : 'S'},
+                                                         {'AttributeName':'EventDay',
+                                                         'AttributeType':'N'}],
+                                    KeySchema=[{'AttributeName':'EventId',
+                                                'KeyType':'HASH'},
+                                                {'AttributeName':'EventDay',
+                                                'KeyType':'RANGE'},
+                                              ],
+                                    ProvisionedThroughput={'ReadCapacityUnits': read_capacity,
+                                                         'WriteCapacityUnits': write_capacity}
+                                  ))
+    except Exception as e:
+        print(str(type(e)))
+        print(e)
+
+def main():
+    table_name='user-visit'
+    create_dynamo_table(table_name,False,1,1)
+
+if __name__ == '__main__':
+    main()
---
+++ readme.md
@@ -0,0 +1,2 @@
+1. Don't forget to install boto3 and aws cli
+2. configure aws with aws configurenn
